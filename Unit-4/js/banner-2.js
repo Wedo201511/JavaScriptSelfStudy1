8cc264dd4ae1a04c2f68a1ec51804fdef4a78e20
Wedo201511 <zziye_@126.com>
2018-04-22 22:26:45 +0800

    轮播图

--- Unit-4/js/banner-2.js
+++ Unit-4/js/banner-2.js
@@ -1,13 +1,14 @@
 (function () {
     var banner = document.getElementById("banner");
-    var bannerInner = utils.getElementsByClassName("inner", banner)[0];
+    var bannerInner = utils.getElementsByClassName("bannerInner", banner)[0];
     var bannerTip = utils.children(banner, "ul")[0];
+    var divList = bannerInner.getElementsByTagName("div");
     var imgList = bannerInner.getElementsByTagName("img");
     var oLis = bannerTip.getElementsByTagName("li");
 
     var bannerLeft = utils.children(banner, "a")[0], bannerRight = utils.children(banner, "a")[1];
     //1.实现数据绑定：AJAX请求数据
-    var jsonData = null, count = null;
+    var jsonData = null;
     ~function () {
         var xhr = new XMLHttpRequest;
         xhr.open("get", "json/banner.txt?=" + Math.random(), false);//random清除浏览器缓存，false是同步请求       
@@ -18,31 +19,20 @@
         };
         xhr.send(null);
     }();
-    //2\、按照字符串拼接的方式绑定数据
+    //2、按照字符串拼接的方式绑定数据
     ~function () {
         //1)绑定轮播图区域的数据
-        var str = "";
+        var str = "", str2 = "";
         if (jsonData) {
             for (var i = 0; i < jsonData.length; i++) {
                 str += '<div><img src="" trueImg="' + jsonData[i]['img'] + '"/></div>';
-
+                i === 0 ? str2 += '<li class="bg"></li>' : str2 += '<li></li>';
             }
-            //把第一张图片复制一份 放到所有图片的末尾（为了实现无缝滚动）
-            str += '<div><img src="" trueImg="' + jsonData[0]['img'] + '"/></div>';
-            count = jsonData.length + 1;
         }
         bannerInner.innerHTML = str;
+        bannerTip.innerHTML = str2;
 
-        utils.css(bannerInner, "width", count * 1000);
-
-        //2)绑定焦点区域的数据     //li的个数也需要动态绑定
-        var str = "";
-        if (jsonData) {
-            for (var i = 0; i < jsonData.length; i++) {
-                i === 0 ? str += '<li class="bg"></li>' : str += '<li></li>';
-            }
-        }
-        bannerTip.innerHTML = str;
+        //
     }();
 
     //3、图片延迟加载
@@ -55,36 +45,27 @@
                 oImg.onload = function () {
                     curImg.src = this.src;
                     curImg.style.display = "block"
-                    //  fadeIn(curImg);
-                    window.zhufengAnimate(curImg, { opacity: 1 }, 300);
+                    //只对第一张图片做处理  z-index: 1;    opacity: 1;
+                    if (i === 0) {
+                        var curDiv = curImg.parentNode;
+                        curDiv.style.zIndex = 1
+                        window.zhufengAnimate(curDiv, { opacity: 1 }, 300);
+                    }
+
                     oImg = null;
                 };
             }(i);
-            var curImg = imgList[i];
-            var oImg = new Image();
-            oImg.src = curImg.getAttribute("trueImg");
-            oImg.onload = function () {
-                curImg.src = this.src;
-                curImg.style.display = "block"
-                //  fadeIn(curImg);
-                // window.zhufengAnimate(curImg, { opacity: 1 }, 500);
-                oImg = null;
-            };
-        }
-
-        curImg.isLoad = true;
+           
+        }        
     }
     window.setTimeout(lazyImg, 500);
 
     //4、自动轮播
     var interval = 1000;
     var autoTimer = window.setInterval(autoMove, interval);
-    var step = 0;//记录步长*当前是哪一张图片，零是第一张图片
+    var step = 0;//记录当前显示的图片的索引
     function autoMove() {
-        //第一张 step=0 运动到  0~-1000
-        //第二张 step=1 运动到-1000~-2000
-        //第三张 step=2 运动到-2000~-3000
-        //第四张 step=3 运动到-3000~0
+
         if (step === count - 1) {
             utils.css(bannerInner, "left", 0);
             step = 0
@@ -129,7 +110,7 @@
         if (step === 0) {
             step = count - 1;
             utils.css(bannerInner, "left", -1000 * step);
-          
+
         }
         step--;
         changeTip();
